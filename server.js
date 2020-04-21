932ed14a159885b7f91c9170887d176c40af8413
annierm18 <annierm18@yahoo.com>
2020-04-21 16:08:14 -0700

    commented out server.js to see what impact itwould have

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
+// const express = require('express');
+// const port = process.env.PORT || 8080;
+// const app = express();
 
-app.use(express.static(__dirname + '/static'));
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/static/index.html');
-})
-app.listen(port);
+// app.use(express.static(__dirname + '/static'));
+// app.get(/.*/, function (req, res) {
+//   res.sendFile(__dirname + '/static/index.html');
+// })
+// app.listen(port);
 
-console.log("server started");
+// console.log("server started");
